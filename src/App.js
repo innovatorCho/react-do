f7223f6d09ece05569049e6c6ecde422dcdd0b97
innovatorCho <spectre1c@gmail.com>
2021-12-07 14:55:33 +0900

    quiz3
    퀴즈 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const quiz = {
+  const [currentNo, setCurrentNo] = useState(0);
+
+  const quizes = [
+    {
+    id : 1,
     question : "일론 머스크의 우주 탐사 기업 이름은?",
     answers : [
       { text : "스페이스 엑스", isCorrect: true},
@@ -10,7 +14,28 @@ function App() {
       { text : "보링 컴퍼니", isCorrect: false},
       { text : "솔라 시티", isCorrect: false}
     ]
-  };
+  },
+  {
+    id : 2,
+    question : "일론 머스크의 고향은 어디일까요?",
+    answers : [
+      { text : "미국 캘리포니아", isCorrect: false},
+      { text : "남아프리가 공화국 프리토리아", isCorrect: true},
+      { text : "캐나다 벤쿠버", isCorrect: false},
+      { text : "호주 시드니", isCorrect: false}
+    ]
+  },
+  {
+    id : 3,
+    question : "일론 머스크가 참업한 페이팔 전신 기업의 이름은?",
+    answers : [
+      { text : "Zip2 Corporation", isCorrect: false},
+      { text : "Alpha Exploration", isCorrect: false},
+      { text : "x.com", isCorrect: true},
+      { text : "Everthing CO.", isCorrect: false}
+    ]
+  },
+];
 
   const handleClick = (isCorrect) => {
     if(isCorrect) {
@@ -18,6 +43,7 @@ function App() {
     } else {
       alert("오답");
     }
+    setCurrentNo((currentNo) => currentNo + 1);
   };
 
   return (
@@ -25,13 +51,13 @@ function App() {
       <div className="app">
         <div className="question-section">
           <h1 className="question-header">
-            <span>1</span>/4
+            <span>{quizes[currentNo].id}</span>/{quizes.length}
           </h1>
-          <div className="question-text">{quiz.question}</div>
+          <div className="question-text">{quizes[currentNo].question}</div>
         </div>
         <div className="answer-section">
           {
-            quiz.answers.map((answer) => (
+            quizes[currentNo].answers.map((answer) => (
               <button onClick={() => handleClick(answer.isCorrect)} value={answer.text}>{answer.text}</button>
             ))
           }
