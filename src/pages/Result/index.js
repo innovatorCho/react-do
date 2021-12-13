adc93104c8bbf36dba16f23987b833cf28368829
innovatorCho <spectre1c@gmail.com>
2021-12-13 13:39:08 +0900

    add result meta

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,15 +1,33 @@
 import {Button, Container, ResultSection} from "components";
 import { useParams } from "react-router";
 
-const Result = ({convertedScore, setScore}) => {
+import { Helmet } from "react-helmet";
+import RESULT from "components/ResultSection/result";
+
+const Result = ({ setScore }) => {
     
     const { cCode } = useParams();
+    const currentUrl = window.document.href;
 
     return (
-        <Container>
-            <ResultSection cCode={cCode}/>
-            <Button to="/" onClick={() => setScore(0)}>테스트 다시하기</Button>
-        </Container>
+        <>
+            {/** URL 정보*/}
+            <meta property="og:url" centent={currentUrl} />
+            {/** title 정보 */}
+            <meta property="og:title" content={RESULT[cCode]["ogTitle"]} />
+            {/** 페이지 상세정보 */}
+            <meta property="og:description" content={RESULT[cCode]["ogDescription"]} />
+            {/** 페이지 대표 이미지 정보 */}
+            <meta property="og:image" content={RESULT[cCode]["ogImage"]} />
+            {/** 트위터 메타 정보 */}
+            <meta property="twitter:title" content={RESULT[cCode]["ogTitle"]} />
+            <meta property="twitter:description" content={RESULT[cCode]["ogDescription"]} />
+            <meta property="twitter:image" conten={RESULT[cCode]["ogImage"]} />
+            <Container>
+                <ResultSection cCode={cCode}/>
+                <Button to="/" onClick={() => setScore(0)}>테스트 다시하기</Button>
+            </Container>
+        </>
     );
 };
 
